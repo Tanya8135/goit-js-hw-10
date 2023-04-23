24dd0e5be13d436feab6eb66e5d3eee78d573738
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2023-04-23 12:13:55 +0300

    hw done

--- src/index.js
+++ src/index.js
@@ -1,89 +1,3 @@
-// import './css/styles.css';
-// import { fetchCountries } from './fetchCountries';
-// import debounce from 'lodash.debounce';
-// import Notiflix from 'notiflix';
-
-// const searchBox = document.querySelector('#search-box');
-// const countryList = document.querySelector('.country-list');
-// const countryInfo = document.querySelector('.country-info');
-// const DEBOUNCE_DELAY = 300;
-
-// searchBox.addEventListener('input', debounce(handleSearchInput, DEBOUNCE_DELAY));
-
-// async function handleSearchInput(evt) {
-//     const searchValue = evt.target.value.trim(); /* введення без пробілів (виключення) */
-//     if (searchValue === '') {
-//         clearCountryList();
-//         clearCountryInfo();
-//         return;
-//     }
-
-//     try {
-//         const countries = await fetchCountries(searchValue);
-
-//         if (countries.length > 10) {
-//             Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
-//             clearCountryList();
-//             clearCountryInfo();
-//             return;
-//         }
-
-//         if (countries.length > 1) {
-//             renderCountryList(countries);
-//             clearCountryInfo();
-//             return;
-//         }
-
-//         if (countries.length === 0) {
-//             Notiflix.Notify.failure('No matches found. Please enter a valid name.');
-//             clearCountryList();
-//             clearCountryInfo();
-//             return;
-//         }
-
-//         // Відображення даних про країну
-//         renderCountryInfo(countries[0]);
-//         clearCountryList();
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// }
-
-// function renderCountryInfo(countryData) {
-//     if (!countryData) {
-//         console.error('Country data is undefined or null');
-//         return;
-//     }
-//     countryInfo.innerHTML = `
-//           <h2>${countryData.officialName || ''}</h2>
-//           <p><strong>Capital:</strong> ${countryData.capital || ''}</p>
-//           <p><strong>Population:</strong> ${countryData.population || ''}</p>
-//           <img src="${countryData.flag || ''}" alt="Flag of ${countryData.officialName || ''}" class="country-flag">
-//           <p><strong>Languages:</strong> ${countryData.languages ? countryData.languages.join(', ') : ''}</p>
-//         `;
-// }
-
-// function clearCountryList() {
-//     countryList.innerHTML = '';
-// }
-
-// function clearCountryInfo() {
-//     countryInfo.innerHTML = '';
-// }
-
-// function renderCountryList(countries) {
-//     countryList.innerHTML = '';
-
-//     countries.forEach(country => {
-//         const countryItem = document.createElement('li');
-//         countryItem.classList.add('country-item');
-//         countryItem.textContent = country.name;
-
-//         countryList.appendChild(countryItem);
-//     });
-// }
-
-
 import './css/styles.css';
 import Notiflix from 'notiflix';
 import debounce from 'lodash.debounce';
@@ -96,7 +10,7 @@ const countryInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
 
 const handleInput = debounce((event) => {
-    const searchTerm = event.target.value.trim(); /* санітизація введеного рядка */
+    const searchTerm = event.target.value.trim();
     if (searchTerm !== '') { /* Перевіряємо, чи не є введене значення порожнім рядком */
         fetchCountries(searchTerm)
             .then(countries => {
@@ -107,13 +21,13 @@ const handleInput = debounce((event) => {
                     updateCountriesList(countries);
                 } else {
                     /* Очищуємо розмітку списку країн
-                            якщо введене значення порожній рядок */
+                       якщо введене значення порожній рядок */
                     clearCountriesList();
                 }
             })
             .catch(err => {
                 if (err.status === 404) {
-                    Notiflix.Notify.failure('Oops, there is no country with that name.');
+                    Notiflix.Notify.failure('Oops, there is no country with that name');
                 } else {
                     console.error(err);
                 }
@@ -131,16 +45,23 @@ function updateCountriesList(countries) {
     countryList.innerHTML = '';
     countries.forEach(country => {
         const countriesItem = document.createElement('div');
-        countriesItem.innerHTML = `<img src="${country.flag}" alt="${country.name}" class="flag"><span>${country.name}</span>`;
+        countriesItem.innerHTML = [
+            `<li class="item-country">
+            <div class="flag-box">
+            <img src="${country.flag}" alt="${country.name}" class="flag">
+            </div>
+            <div class="info-box">
+            <h2 class="title-name">${country.name}</h2>
+            <p class="info-text">${country.capital}</p>
+            <p class="info-text">${country.population}</p>
+            <p class="info-text">${country.languages}</p>
+            </div>
+            </li>`].join('');
         countryList.appendChild(countriesItem);
     });
 }
 
-// Використання функції fetchCountries:
-fetchCountries('Ukraine')
-    // .then(countries => {
-    //     console.log(countries);
-    // })
-    // .catch(err => {
-    //     console.error(err);
-    // });
+// Видалення списку країн
+function clearCountriesList() {
+    countryList.innerHTML = '';
+}
